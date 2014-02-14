a462274f9cedcd6fb0557559d9bdd091879020ec
Logan Hasson <logan.hasson@gmail.com>
2014-02-13 19:55:47 -0500

    Meh

--- tweet.rb
+++ tweet.rb
@@ -11,9 +11,20 @@ class Tweet
 
   def get_neighbors
     puts "Getting neighbors for: #{username}"
+    # current_children = []
+    search_params = {
+      count: 100
+    }
 
-    TwitterLinkChain::CLIENT.search(id).reject {|t| t.retweet? == true}.map do |tweet|
-      screen_name = tweet.user.screen_name
+    search = TwitterLinkChain::CLIENT.search(id, search_params)
+
+    while next_params = search.send("next_page")
+      new_params = search_params.merge(next_params)
+      search += TwitterLinkChain::CLIENT.search(id, new_params)
+    end
+
+    search.reject {|t| t.user.screen_name == username}.map do |tweet|
+      username = tweet.user.screen_name
       id = tweet.id
       created_at = tweet.created_at
       location = nil
@@ -21,8 +32,15 @@ class Tweet
         location = tweet.place
       end
 
-      Tweet.new(screen_name, id, created_at, location)
+      # current_children << username
+
+      # if current_children.select {|c| c == username}.size == 1
+      Tweet.new(username, id, created_at, location)
+      # else
+      #   nil
+      # end
     end
+    # .compact
   end
 
 end
